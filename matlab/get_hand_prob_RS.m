cbb4119e189dafff859863628335f1b530db6b7f
Chuan Qin <chuanguo_learn@163.com>
2021-07-16 10:33:15 +0800

    srm prob

--- matlab/get_hand_prob_RS.m
+++ matlab/get_hand_prob_RS.m
@@ -1,14 +1,17 @@
-function get_hand_prob_RS(feature_path, clf_path, eval_names)
-    if nargin < 3
-        eval_names = './testing_names.mat'
-    end
-    %% load features
+function get_hand_prob_RS(feature_path, clf_path, ref_tst_dir)
     feature = matfile(feature_path);
     F = feature.F; 
     full_names = feature.names; 
-    eval_names = matfile(eval_names); eval_names = eval_names.testing_names;
-    names = full_names(ismember(full_names, eval_names));
-    F = F(ismember(full_names, eval_names),:);
+    names = feature.names; 
+    if nargin == 3
+        tst_items = dir(ref_tst_dir); tst_items = tst_items(3:end);
+        tst = cell(length(tst_items),1);
+        for i=1:length(tst_items)
+            tst{i,1} = tst_items(i).name;
+        end
+        names = full_names(ismember(full_names, tst));
+        F = F(ismember(full_names, tst),:);
+    end
 
     %% extract features
     % [F, names] = extract_feature_demo(file_dir);
